ef7e2514b480a31a4303465b1d53922493c626d3
Sanyam Miglani <122440583+sanyammiglani36@users.noreply.github.com>
2024-04-04 23:41:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+//async function
+
+// async function abcd(){
+//     return 7;
+// }
+// abcd();
+
+
+//async function
+// async function utility(){
+
+
+
+//     let haryanaMausam = new Promise((resolve,reject) => {
+//         setTimeout(() => {
+//             resolve("Haryana me garmi hai");
+//         },10000);
+//     });
+
+//     let delhiMausam = new Promise((resolve,reject) => {
+//         setTimeout(() => {
+//             resolve("Delhi me bhot hi jyada garmi hai");
+//         },20000);
+//     });
+
+//     let hM = haryanaMausam;
+//     let dM = delhiMausam;
+
+//     return [hM,dM];
+
+// }
+
+//await function
+
+// async function utility(){
+
+
+
+//     let haryanaMausam = new Promise((resolve,reject) => {
+//         setTimeout(() => {
+//             resolve("Haryana me garmi hai");
+//         },10000);
+//     });
+
+//     let delhiMausam = new Promise((resolve,reject) => {
+//         setTimeout(() => {
+//             resolve("Delhi me bhot hi jyada garmi hai");
+//         },20000);
+//     });
+
+//     let hM = await haryanaMausam;
+//     let dM = delhiMausam;
+
+//     return [hM,dM];
+
+// }
